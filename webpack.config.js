187d9ae2800744bfeb8bc7789920ae4722f221d0
Konstantin Lukyanenok <loukianen@gmail.com>
2021-01-04 12:36:26 +0300

    fixed entry point

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/app/index.js',
+  entry: './src/index.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
